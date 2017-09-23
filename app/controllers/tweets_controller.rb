d64126cf990c54fa447576511ba59f65fa001c6d
seishiro1216gitzaki <canoe1216@gmail.com>
2017-09-23 12:42:36 +0900

    add index action & create action to tweets_controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,16 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
   def index
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new
+  end
+
+
+  private
+  def create_params
+    params.require(:tweet).permit(:content, :image)
   end
 end
