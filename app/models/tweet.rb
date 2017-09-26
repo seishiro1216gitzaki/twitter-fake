23507bac349f6c8e6a55a1cd7bb5af804dee0f75
seishiro1216gitzaki <canoe1216@gmail.com>
2017-09-26 22:47:43 +0900

    delete imageuploader method from tweet.rb

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,5 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  mount_uploader :image, ImageUploader
   validates :content, presence: true
   validates :image, presence: true, if: -> { content.blank? }
 end
