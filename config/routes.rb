8d8f56f0d07a8f89f0804caddd9c0203600b3646
seishiro1216gitzaki <canoe1216@gmail.com>
2017-10-03 19:50:49 +0900

    add routing for follow

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tweets#index'
-  resources :users, only: [:index,:edit, :update]
+  resources :users, only: [:index,:edit, :update] do
+    member do
+       get :following, :followers
+    end
   resources :tweets, only: [:index, :create]
 end
