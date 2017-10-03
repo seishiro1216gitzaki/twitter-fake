c3a2e70b19a244d092c0413afe68e67a7dbc621e
seishiro1216gitzaki <canoe1216@gmail.com>
2017-10-03 19:25:40 +0900

    add method
    add method about follow, unfollow,if following return true

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         has_many :tweets
-         has_many :active_relationships,class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :tweets
+
+  has_many :active_relationships,class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :following
+
+  has_many :passive_relationships,class_name:  "Relationship", foreign_key: "following_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(following_id: other_user.id)
+  end
+  # ユーザーをアンフォローする
+  def unfollow(other_user)
+    active_relationships.find_by(following_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
