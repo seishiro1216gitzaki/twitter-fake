fd2b1608ef25034d65bf13bf6a7c1eb0997b68f6
seishiro1216gitzaki <canoe1216@gmail.com>
2017-10-05 20:29:24 +0900

    to get user that followed
    user.followingsをできるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ApplicationRecord
   has_many :tweets
 
   has_many :following_relationships,class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :following_relationships
+
+  has_many :follower_relationships,class_name:  "Relationship", foreign_key: "following_id", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+
 
 
 end
