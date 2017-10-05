b7a8ff60a6394e562a4265c41f667c4566be88b0
seishiro1216gitzaki <canoe1216@gmail.com>
2017-10-05 20:24:11 +0900

    to get user that can follow
    user.following_relationships.followingsをできるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,23 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets
 
-  has_many :active_relationships,class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :following, through: :active_relationships, source: :following
+  has_many :following_relationships,class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
 
-  has_many :passive_relationships,class_name:  "Relationship", foreign_key: "following_id", dependent: :destroy
-  has_many :followers, through: :passive_relationships, source: :follower
 
-  # ユーザーをフォローする
-  def follow(other_user)
-    active_relationships.create(following_id: other_user.id)
-  end
-  # ユーザーをアンフォローする
-  def unfollow(other_user)
-    active_relationships.find_by(following_id: other_user.id).destroy
-  end
-
-  # 現在のユーザーがフォローしてたらtrueを返す
-  def following?(other_user)
-    following.include?(other_user)
-  end
 end
