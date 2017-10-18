a2df944a0eaef02988e9956bdd95f8dfdb44e21e
seishiro1216gitzaki <canoe1216@gmail.com>
2017-10-18 20:07:29 +0900

    add imageuploader to tweet.rb

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
   validates :image, presence: true, if: -> { content.blank? }
+  mount_uploader :image, ImageUploader
 end
