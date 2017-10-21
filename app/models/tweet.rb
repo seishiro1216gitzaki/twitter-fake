fcd127a8fb9ec7b8b1382cd646f85ae433dfc11b
seishiro1216gitzaki <canoe1216@gmail.com>
2017-10-21 15:12:41 +0900

    add association to tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+  has_many :watch_laters
   validates :content, presence: true
   validates :image, presence: true, if: -> { content.blank? }
   mount_uploader :image, ImageUploader
