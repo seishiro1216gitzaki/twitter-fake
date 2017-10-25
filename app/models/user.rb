0646b2f5cda6cb5b67d98827c1e4c5151bfc5104
seishiro1216gitzaki <canoe1216@gmail.com>
2017-10-25 19:36:31 +0900

    add association user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :tweets, through: :watch_laters
+  has_many :tweets
   has_many :watch_laters
 
   has_many :following_relationships,class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
