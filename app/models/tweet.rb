09664faf8cf866bc88339088382bcaa7357044aa
seishiro1216gitzaki <canoe1216@gmail.com>
2017-10-28 19:20:53 +0900

    add source to has_many watch_laters_users

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :watch_laters
-  has_many :watch_laters_users, through: :watch_laters
+  has_many :watch_laters_users, through: :watch_laters, source: :tweet
   validates :content, presence: true
   validates :image, presence: true, if: -> { content.blank? }
   mount_uploader :image, ImageUploader
